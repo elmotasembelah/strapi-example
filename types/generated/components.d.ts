6b56510c5d1923068112c477fa8a8e9629eafcc2
elmoatasm <elmoatasm-belah@spatiumsoftware.com>
2024-05-12 09:15:02 +0300

    initial commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,73 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface BlocksHero extends Schema.Component {
+  collectionName: 'components_blocks_heroes';
+  info: {
+    displayName: 'Hero';
+  };
+  attributes: {
+    heading: Attribute.String;
+    text: Attribute.String;
+    link: Attribute.Component<'elements.button-link'>;
+    image: Attribute.Media;
+  };
+}
+
+export interface BlocksRow extends Schema.Component {
+  collectionName: 'components_blocks_rows';
+  info: {
+    displayName: 'Row';
+  };
+  attributes: {
+    card: Attribute.Component<'elements.card', true>;
+  };
+}
+
+export interface ElementsButtonLink extends Schema.Component {
+  collectionName: 'components_elements_button_links';
+  info: {
+    displayName: 'Button Link';
+  };
+  attributes: {
+    title: Attribute.String;
+    link: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+    type: Attribute.Enumeration<['PRIMARY', 'SECONDARY']>;
+  };
+}
+
+export interface ElementsCard extends Schema.Component {
+  collectionName: 'components_elements_cards';
+  info: {
+    displayName: 'card';
+  };
+  attributes: {
+    heading: Attribute.String;
+    description: Attribute.Text;
+    image: Attribute.Media;
+  };
+}
+
+export interface SeoMetaData extends Schema.Component {
+  collectionName: 'components_seo_meta_data';
+  info: {
+    displayName: 'Meta Data';
+  };
+  attributes: {
+    metaTitle: Attribute.String;
+    metaDescription: Attribute.Text;
+    metaImage: Attribute.Media;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'blocks.hero': BlocksHero;
+      'blocks.row': BlocksRow;
+      'elements.button-link': ElementsButtonLink;
+      'elements.card': ElementsCard;
+      'seo.meta-data': SeoMetaData;
+    }
+  }
+}
